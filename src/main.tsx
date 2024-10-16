87250fa2bf46c504b75d79f8c5081ad4f97423ea
Luat Huynh <luat.huynh@cyberlogitec.com>
2024-10-16 10:20:41 +0700

    3

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 //A_1
+//A_2
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
