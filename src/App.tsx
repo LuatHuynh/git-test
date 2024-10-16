626498faa0abff13a756e4d92b1533d767021e10
Luat Huynh <luat.huynh@cyberlogitec.com>
2024-10-16 10:20:14 +0700

    2

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+//B_2
 function App() {
   const [count, setCount] = useState(0)
 
